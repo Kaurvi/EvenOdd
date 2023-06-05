33e3b7a207a49578d88d95ad536c5d3bc00aa9e6
Kaurvi <kaurvi@mail.uc.edu>
2023-06-04 20:07:14 -0400

    code added

---
+++ src/EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd
+{
+    public static void main(String[] args)
+    {
+     int num = 2;
+     int remainder = num%2;
+     if (remainder == 1)
+         System.out.println("Odd Number");
+     else
+         System.out.println("Even Number");
+    }
+}
